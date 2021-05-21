20ecd13d8dd77dc1cdb8590de9f892b57fa2ed09
Elvis Landi <elandi13@yahoo.com>
2021-05-21 09:58:00 -0400

    created a counter for practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import { useTranslation } from 'react-i18next';
 import './App.css';
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+
 
 function App() {
   const { t, i18n } = useTranslation();
+ 
+  const [counter, setCounter] = useState(0)
+
+
 
   function handleClick(lang){
     i18n.changeLanguage(lang)
   }
+  
+
 
 
   return (
@@ -21,6 +30,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p> {t('Hello.1')}</p>
         <p> {t('Text.1')}</p>
+        <p> {counter}</p>
+        <button onClick={()=> {
+          setCounter(counter + 1)
+        }}> Increase Counter </button>
       </header>
     </div>
   );
---
+++ src/Fetch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Fetch(api) {
+    // const listApi = api.map((data) =>
+    //   <li>{data.gender}</li>
+    // );
+    // console.log(listApi)
+
+    return (
+        <div>
+            this is where my api is going to go
+        </div>
+    )
+}
